959d0bce384579f80701a91dc1b3f79f1df075fe
Naufal Muzakki <muhammadnaufal9595@gmail.com>
2020-08-26 23:02:14 +0700

    second commit

---
+++ Test Suites/coba1.ts
@@ -0,0 +1,25 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description>cobacobatestsuite</description>
+   <name>coba1</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>0</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <testSuiteGuid>ac441268-ae05-4cdd-b3ce-226512a301f3</testSuiteGuid>
+   <testCaseLink>
+      <guid>ad5b42f3-834d-4dbe-9b9c-312859e0b6e2</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Jawa Timur/TP-001_pilih berbagai gunung di jawa timur</testCaseId>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>70ab435e-56d3-42d0-b808-368caec6588c</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/Jawa Tengah/TP-002_pilih berbagai gunung di jawa tengah</testCaseId>
+   </testCaseLink>
+</TestSuiteEntity>
